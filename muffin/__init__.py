49cd7c7611e2d8c942968f6448e1ac6d74dc94bd
Kirill Klenov <horneds@gmail.com>
2015-03-13 18:03:24 +0300

    Bump version: 0.0.21 → 0.0.22

--- muffin/__init__.py
+++ muffin/__init__.py
@@ -6,7 +6,7 @@
 # Package information
 # ===================
 
-__version__ = "0.0.21"
+__version__ = "0.0.22"
 __project__ = "muffin"
 __author__ = "Kirill Klenov <horneds@gmail.com>"
 __license__ = "MIT"
