bce54aea34501799c292a1385a26c7ae62355f10
Kirill Klenov <horneds@gmail.com>
2021-03-24 16:24:15 +0300

    build(version): 0.63.2 -> 0.64.0

--- muffin/__init__.py
+++ muffin/__init__.py
@@ -3,7 +3,7 @@
 # Package information
 # ===================
 
-__version__ = "0.63.2"
+__version__ = "0.64.0"
 __project__ = "muffin"
 __author__ = "Kirill Klenov <horneds@gmail.com>"
 __license__ = "MIT"
