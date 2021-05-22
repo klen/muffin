544c34e9b436c021bd6dffde348179f67e7cbb07
Kirill Klenov <horneds@gmail.com>
2021-05-22 20:18:04 +0300

    feat: update modconfig

--- tests/test_pytest.py
+++ tests/test_pytest.py
@@ -1,3 +1,11 @@
-async def test_app_imported(app):
+import pytest
+
+
+@pytest.fixture
+def name(app):
+    return app.cfg.name
+
+
+async def test_app_imported(app, name):
     assert app.cfg.name == 'muffin'
     assert app.state == 'started'
