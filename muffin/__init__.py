8d088edf1d0070a1219d8f672eed23d7852b28ca
Kirill Klenov <horneds@gmail.com>
2021-06-23 14:17:57 +0300

    build(version): 0.81.0 -> 0.82.0

--- muffin/__init__.py
+++ muffin/__init__.py
@@ -3,7 +3,7 @@
 # Package information
 # ===================
 
-__version__ = "0.81.0"
+__version__ = "0.82.0"
 __project__ = "muffin"
 __author__ = "Kirill Klenov <horneds@gmail.com>"
 __license__ = "MIT"
